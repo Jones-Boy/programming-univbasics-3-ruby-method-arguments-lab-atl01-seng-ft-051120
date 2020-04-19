47f031cbdc6c7f454fb9516982f582c0778ad56b
Michael Jones <djyoungjones@gmail.com>
2020-04-19 17:09:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@ end
 introduction 
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learing to progam in #{language)."
+  puts "Hi, my name is #{name} and I am learinng to progam in #{language}."
 end 
 
 introduction_with_language
